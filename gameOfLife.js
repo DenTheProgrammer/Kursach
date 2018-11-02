344c86bf12de4df009f4b72e3a6504aae95e2963
DenTheProgrammer <den4ikk007@gmail.com>
2018-11-03 01:54:45 +0300

    getSiblings

--- gameOfLife.js
+++ gameOfLife.js
@@ -21,8 +21,7 @@ function renderField(x,y) {
        for (let j=0;j<x;j++){
            let cell=document.createElement("div");
            cell.className="field__cell";
-           cell.setAttribute("data-cordX",j+"");
-           cell.setAttribute("data-cordY",i+"");
+           cell.id=`x${j}y${i}`;//id with cords
             rowOfCells.appendChild(cell);
        }
        field.appendChild(rowOfCells);
@@ -42,9 +41,29 @@ field.addEventListener("click",(e)=>{
 
 function nextStep(){
     let aliveCells=document.querySelectorAll(".field__cell_alive");//returns NodeList
-    console.log(aliveCells);
+    // console.log(aliveCells);
     for(let i=0;i<aliveCells.length;i++){
-        console.log(aliveCells[i].getAttribute("data-cordX"));
+        // console.log(aliveCells[i].getAttribute("id"));
+        getSiblings(aliveCells[i]);
     }
 }
+
+
+function getSiblings(cell){
+    const cellCordX=parseInt(cell.getAttribute("id")[1]),
+        cellCordY=parseInt(cell.getAttribute("id")[3]);
+    let cellSiblings=[document.getElementById(`x${cellCordX}y${cellCordY-1}`),
+        document.getElementById(`x${cellCordX+1}y${cellCordY-1}`),
+        document.getElementById(`x${cellCordX+1}y${cellCordY}`),
+        document.getElementById(`x${cellCordX+1}y${cellCordY+1}`),
+        document.getElementById(`x${cellCordX}y${cellCordY+1}`),
+        document.getElementById(`x${cellCordX-1}y${cellCordY+1}`),
+        document.getElementById(`x${cellCordX-1}y${cellCordY}`),
+        document.getElementById(`x${cellCordX-1}y${cellCordY-1}`)];
+        //TODO do not run filter for all elements
+        cellSiblings=cellSiblings.filter((elem)=>elem!==null);
+
+    return cellSiblings;
+}
 renderField(GameInfo.fieldSize.x,GameInfo.fieldSize.y);
+
